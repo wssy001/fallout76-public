630b47ddec28846e628750da33daa4e8ca212315
zhaojiahao <peter003@foxmail.com>
2023-05-15 13:22:53 +0800

    feat: 新增wiki基础服务

---
+++ wiki-provider/src/main/java/cyou/wssy001/wikiprovider/service/WikiServiceImpl.java
@@ -0,0 +1,14 @@
+package cyou.wssy001.wikiprovider.service;
+
+import cyou.wssy001.common.service.WikiService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: Wiki服务实现类
+ * @Author: Tyler
+ * @Date: 2023/5/15 10:24
+ * @Version: 1.0
+ */
+@Service
+public class WikiServiceImpl implements WikiService {
+}
